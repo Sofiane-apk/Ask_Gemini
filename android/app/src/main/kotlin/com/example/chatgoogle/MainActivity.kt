397280a9c73e22ef2367184b55ed31f999bd4202
Sofiane <maaradji.sofiane@gmail.com>
2024-02-21 22:17:57 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/chatgoogle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatgoogle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/google.gemini.dart
@@ -0,0 +1,4 @@
+library google_gemini;
+
+export 'package:chatgoogle/src/models/gemini/gemini.dart';
+export 'package:chatgoogle/src/repository/apis.dart';
